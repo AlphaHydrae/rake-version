f29b829fe1085fe324b746f414d1ef1f5f5cd0f5
AlphaHydrae <hydrae.alpha@gmail.com>
2012-02-21 12:48:47 +0100

    Spec helper.

---
+++ spec/helper.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'bundler'
+require 'simplecov'
+
+# test coverage
+SimpleCov.start
+
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
+require 'rspec'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rake-version'
+
